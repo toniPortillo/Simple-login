3454735011c2ae308d15b5295d8afd6994be7554
toniPortillo <toniplsec@gmail.com>
2019-11-03 22:09:39 +0100

    Primer commit del login sencillo, con la primera version

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,50 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link v-if="authenticated" to="/login" v-on:click.native="logout()" 
+      replace>Logout</router-link>
     </div>
-    <router-view/>
+    <router-view @authenticated="setAuthenticated"/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      authenticated: false,
+      mockAccount: {
+        username: "toni"
+      }
+    }
+  },
+  mounted() {
+    if(!this.authenticated) {
+      this.$router.replace({ name: "login" });
+    }
+  },
+  methods: {
+    setAuthenticated(status) {
+      this.authenticated = status
+    },
+    logout() {
+      this.authenticated = false;
+    }
+  }
 }
+</script>
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style scoped>
+  body {
+    background-color: #F0F0F0;
+  }
+  h1 {
+    padding: 0;
+    margin-top: 0;
+  }
+  #app {
+    width: 1024px;
+    margin: auto;
+  }
 </style>
---
+++ src/views/login.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="login">
+        <h1>Login</h1>
+        <input type="text" name="username" v-model="input.username" placeholder="Username" />
+        <button type="button" v-on:click="login()">Login</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Login',
+        data() {
+            return {
+                input: {
+                    username: ""
+                }
+            }
+        },
+        methods: {
+            login() {
+                if(this.input.username != "") {
+                    if(this.input.username == this.$parent.mockAccount.username) {
+                        this.$emit("authenticated", true);
+                        this.$router.replace({ name: "secure" });
+                    }   else {
+                        //console.log("The username is incorrect");
+                    }
+                } else {
+                    //console.log("A username must be present")
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    #login {
+        width: 500px;
+        border: 1px solid #CCCCCC;
+        background-color: #FFFFFF;
+        margin: auto;
+        margin-top: 200px;
+        padding: 20px;
+    }
+</style>
